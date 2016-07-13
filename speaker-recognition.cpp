4d5df71c6e433849cb39e0220e322caebcbce09b
Ben Zhang <benzh@cs.berkeley.edu>
2016-07-13 10:43:53 -0700

    Add testing

--- speaker-recognition.cpp
+++ speaker-recognition.cpp
@@ -1,6 +1,7 @@
 #include <dirent.h>
 #include <GRT/GRT.h>
 #include <iostream>
+#include <limits>
 
 #include "mfcc.h"
 #include "wav-reader.h"
@@ -12,13 +13,16 @@ bool load_audio_file_from_directory(
     GRT::TimeSeriesClassificationData& data,
     const char* const directory,
     uint32_t label,
-    uint32_t count = numeric_limits<uint32_t>::max()) {
+    uint32_t count = numeric_limits<uint32_t>::max(),
+    uint32_t skip = 0) {
     DIR *dir;
     struct dirent *ent;
     if ((dir = opendir(directory)) != NULL) {
         while ((ent = readdir(dir)) != NULL) {
             char* suffix = strrchr(ent->d_name, '.');
             if (suffix != NULL && 0 == strcmp(suffix, ".wav")) {
+                if (skip > 0) { skip -= 1; continue; }
+
                 printf("loading from %s\n", ent->d_name);
 
                 std::string path;
@@ -47,17 +51,30 @@ int main(int argc, char* argv[]) {
 
     // Sample rate is 16k, FFT window size 512 => 32 ms, hopsize 128 => 8 ms
     GRT::FFT fft(512, 128, 1,  GRT::FFT::HAMMING_WINDOW, true, false);
-    GRT::MFCC mfcc(16000, 512 / 2, 300, 8000, 26, 12, 22);
-    GRT::GMM gmm(32, true, true, 0.5);
+    GRT::MFCC mfcc(16000, 512 / 2, 300, 8000, 26, 5, 22);
+    GRT::GMM gmm(5, true, true, 0.5);
 
     pipeline.addFeatureExtractionModule(fft);
     pipeline.addFeatureExtractionModule(mfcc);
     pipeline.setClassifier(gmm);
 
     GRT::TimeSeriesClassificationData data(1);
-    load_audio_file_from_directory(data, kFemaleDatasetDir, 1, 30);
-    load_audio_file_from_directory(data, kMaleDatasetDir, 2, 30);
+    load_audio_file_from_directory(data, kFemaleDatasetDir, 1, 1);
+    load_audio_file_from_directory(data, kMaleDatasetDir, 2, 1);
     std::cout << data.getStatsAsString() << std::endl;
 
     pipeline.train(data);
+
+    // GRT::TimeSeriesClassificationData test_data(1);
+    // load_audio_file_from_directory(test_data, kFemaleDatasetDir, 1, 1, 30);
+
+    // GRT::MatrixDouble test_samples = test_data.getDataAsMatrixDouble();
+    // vector<uint32_t> classified_result(2, 0);
+    // for (uint32_t i = 0; i < test_samples.getNumRows(); i++) {
+    //     classified_result[pipeline.predict(test_samples.getRowVector(i))]++;
+    // }
+
+    // for (const auto& i : classified_result) {
+    //     std::cout << i << std::endl;
+    // }
 }
